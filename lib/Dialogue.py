cef0c4de94c0249f637c54ace50fd6db85eabb0b
Antoine GICQUEL <antoine.gicquel@telecom-paris.fr>
2020-03-10 13:40:34 +0100

    Fix bug dans le Dialogue où l'image du personnage n'aparaissait pas

--- lib/Dialogue.py
+++ lib/Dialogue.py
@@ -6,11 +6,13 @@ class Dialogue(object):
     def show(self):
         global fenetre, window_dimensions
         margin = 10
-        scaleFactor = 10
+        scaleFactor = 4
         font = pygame.font.Font("./res/fonts/verdana.ttf", 30)
         text_height = blit_text(fenetre, self.text, (2*margin, 0), window_dimensions[0] - 2*margin, font, False)
         yTop = window_dimensions[1] - margin - text_height
         pygame.draw.rect(fenetre, (100,0,100) , (margin, yTop, window_dimensions[0] - 2*margin, window_dimensions[1] - yTop - margin)) # on met le background du dialogue
-        fenetre.blit(pygame.transform.scale(self.sprite, (scaleFactor,scaleFactor)), (margin, yTop - scaleFactor * 40))# on met l'image du perso qui parle
+        
+        fenetre.blit(pygame.transform.scale(self.sprite, (scaleFactor*40,scaleFactor*40)), (margin, yTop - scaleFactor * 40))# on met l'image du perso qui parle
+        # fenetre.blit(self.sprite, (margin, yTop -))# on met l'image du perso qui parle
         blit_text(fenetre, self.text, (2*margin, yTop), window_dimensions[0] - 2*margin, font, True, pygame.Color("white"))
         pygame.display.flip()
